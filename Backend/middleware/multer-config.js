fd576ada0f9d7cf6dc9c790be346e5ad6d9c858c
Liaosha <alyosasha@gmail.com>
2024-02-22 18:10:23 +0100

    Ajout de multer-config

---
+++ Backend/middleware/multer-config.js
@@ -0,0 +1,22 @@
+//Importer multer
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+//Créer un objet de configuration pour multer. diskStorage=on va l'enregistrer sur le disk.
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => { //quelle dossier enregistrer les dossiers
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({ storage }).single('image');
